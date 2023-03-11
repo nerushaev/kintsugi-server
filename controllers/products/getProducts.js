2a8e5bbb65ae0008297524b8735cfb7ef6160be7
Nikolay Chereliuk <kolyanerushaev@gmail.com>
2023-03-11 23:41:59 +0200

    update getProducts controller

--- controllers/products/getProducts.js
+++ controllers/products/getProducts.js
@@ -2,36 +2,26 @@ const Product = require('../../models/product');
 
 const getProducts = async (req, res) => {
   const { page = 1, limit = 6, category } = req.query;
+  const categories = category ? category.split(",") : [];
 
-  if (category) {
-    const count = await Product.countDocuments({ category });
-    console.log(count);
-    const products = await Product.find({ category, comingSoon: { $exists: false } })
-      .limit(limit * 1)
-      .skip((page - 1) * limit)
-      .exec()
-    
-      res.json({
-    products,
-    totalPages: Math.ceil(count / limit),
-    currentPage: page
-  });
-  } else {
-    const count = await Product.countDocuments({ comingSoon: { $exists: false } });
-    const products = await Product.find({ comingSoon: { $exists: false } })
-      .limit(limit * 1)
-      .skip((page - 1) * limit)
-      .exec();
-      res.json({
-    products,
-    totalPages: Math.ceil(count / limit),
-    currentPage: page
-  });
+  const query = {
+    comingSoon: { $exists: false }
+  };
+  if (categories.length > 0) {
+    query.category = { $in: categories };
   }
+  console.log(query);
 
-
-  
-
+  const count = await Product.countDocuments(query);
+  const products = await Product.find(query)
+    .limit(limit * 1)
+    .skip((page - 1) * limit)
+    .exec();
+  res.json({
+    products,
+    totalPages: Math.ceil(count / limit),
+    currentPage: page
+  });
 };
 
 module.exports = getProducts;
