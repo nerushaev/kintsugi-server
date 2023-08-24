504d2cfba5d0f13ba44ac99c064ccba47450307f
Nikolay Chereliuk <kolyanerushaev@gmail.com>
2023-08-24 15:24:06 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,10 +5,13 @@ require("dotenv").config();
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
-const origin =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:3000"
-    : "https://www.kintsugi.org.ua";
+const origin = "https://www.kintsugi.org.ua";
+
+// const origin =
+//   process.env.NODE_ENV === "development"
+//     ? "http://localhost:3000"
+//     : "https://www.kintsugi.org.ua";
+console.log(origin);
 // const origin =
 //   // process.env.NODE_ENV === "development"
 //     // ? "http://localhost:3000"
